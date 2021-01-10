2c728457250276664eb252c4c74b3fcf75a7737a
Hans Klysner <hans@klysner.dk>
2021-01-10 14:54:02 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        }
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+        }
+    }
+})
